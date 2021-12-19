fe32a6de11dfee7f2c77d5f5c62f5551fdbd0ad4
CIdsanto <cidsantos56@gmail.com>
2021-12-18 22:06:34 -0600

    Logout
    -Con la funcion de Django logout se agrego boton al navbar para cerrar sesion.

--- Estacionamiento/usuarios/views.py
+++ Estacionamiento/usuarios/views.py
@@ -16,3 +16,8 @@ def login_user(request):
             return redirect('login')      
     else:
         return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Has cerrado Sesion"))
+    return redirect('login')
