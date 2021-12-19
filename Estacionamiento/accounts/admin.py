1cdd00611f4714e2b59f581b3b7a5d7cd50fe8ab
CIdsanto <cidsantos56@gmail.com>
2021-12-19 04:03:22 -0600

    2
    d

--- Estacionamiento/accounts/admin.py
+++ Estacionamiento/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tipo
+from .models import Clientes
+# from .models import Slots
 
-# Register your models here.
+# from .models import Salidas
+# admin.site.register(Slots)
+admin.site.register(Tipo)
+admin.site.register(Clientes)
+# admin.site.register(Salidas)
